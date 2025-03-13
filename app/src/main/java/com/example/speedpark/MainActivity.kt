f8bc20b76fd4605bff018a22170839102a93ba6d
Miles Pulliam <milespulliam@gmail.com>
2025-03-13 15:11:56 -0400

    Created home page, need to add functionality to login and signup buttons

--- app/src/main/java/com/example/speedpark/MainActivity.kt
+++ app/src/main/java/com/example/speedpark/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.speedpark
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var signupBut: Button
+    private lateinit var loginBut: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
+        signupBut.setOnClickListener{
+
+            val intent = Intent(this,CreateAccount::class.java)
+            startActivity(intent)
+        }
+
+        loginBut.setOnClickListener{
+
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+        }
     }
-}
