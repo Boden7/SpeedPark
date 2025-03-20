d0545b28926e27e8ab021ad345988883189ccec9
Boden7 <bakahn973@comcast.net>
2025-03-20 15:47:48 -0400

    Started to add firebase login

--- app/src/main/java/com/example/speedpark/CreateAccount.kt
+++ app/src/main/java/com/example/speedpark/CreateAccount.kt
@@ -7,12 +7,20 @@ package com.example.speedpark
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class CreateAccount : AppCompatActivity() {
+    private lateinit var emailInput: EditText
+    private lateinit var passwordInput: EditText
+    private lateinit var confirmPasswordInput: EditText
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,16 +31,42 @@ class CreateAccount : AppCompatActivity() {
             insets
         }
 
+        emailInput = findViewById(R.id.createEmail)
+        passwordInput = findViewById(R.id.createPassword)
+        confirmPasswordInput = findViewById(R.id.createPassword2)
+        auth= FirebaseAuth.getInstance()
         // Make the create account button
         val createButton = findViewById<Button>(R.id.createButton)
         createButton.setOnClickListener{
+            val email = emailInput.text.toString()
+            val password = passwordInput.text.toString()
+            val confirmPassword = confirmPasswordInput.text.toString()
+            // Make sure fields are not empty
+            if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()){
+                Toast.makeText(this, "Please fill out all fields", Toast.LENGTH_SHORT).show()
+            }
             // Make sure passwords match
-
+            if (password != confirmPassword){
+                // Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show()
+                confirmPasswordInput.error = "Passwords do not match"
+            }
             // Enter data in the user database
-
-            // Log in
-
-            // Send them back to the home page, where they can log in with their new account
+            else{
+                confirmPasswordInput.error=null
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this){task ->
+                    if (task.isSuccessful) {
+                        // Send them back to the home page, where they can log in with their new account
+                        Toast.makeText(this, "Sign up successful", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else{
+                        Toast.makeText(this, "Sign up failed: ${task.exception?.message}",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            // Send them back to the home page
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/example/speedpark/LoginPage.kt
+++ app/src/main/java/com/example/speedpark/LoginPage.kt
@@ -7,13 +7,19 @@ package com.example.speedpark
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginPage : AppCompatActivity() {
     private lateinit var buttonLogin: Button
+    private lateinit var auth: FirebaseAuth
+    private lateinit var emailInput: EditText
+    private lateinit var passwordInput: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,25 +30,41 @@ class LoginPage : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        // Create the login button
+        auth = FirebaseAuth.getInstance()
+        emailInput = findViewById(R.id.Inemail)
+        passwordInput = findViewById(R.id.InPassword)
         buttonLogin = findViewById(R.id.buttonLogin)
 
         // Set listener to check for button clicks
         buttonLogin.setOnClickListener{
             // Validate login credentials
-
-            // Determine if the user is an admin or not
-            val isAdmin = true // temporary
-            // Go to the admin page
-            if(isAdmin) {
-                val intent = Intent(this, AdminView::class.java)
-                startActivity(intent)
+            val email = emailInput.text.toString()
+            val password = passwordInput.text.toString()
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please fill out all fields",
+                    Toast.LENGTH_SHORT).show()
+            } else {
+                loginUser(email, password)
             }
-            // Go to the user page
-            else{
-                val intent = Intent(this, UserView::class.java)
-                startActivity(intent)
+        }
+    }
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+                val admin = false
+                // determine if the user is an admin or not
+                if (admin) {
+                    // Proceed to the admin screen
+                    val intent = Intent(this, AdminView::class.java)
+                    startActivity(intent)
+                } else {
+                    // Proceed to the user screen
+                    val intent = Intent(this, UserView::class.java)
+                    startActivity(intent)
+                }
+            } else {
+                Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
             }
         }
     }
